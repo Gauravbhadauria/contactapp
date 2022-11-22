b1c7d0b3fd5a140bd97898df319819a0b50815e9
Gaurav Bhadauria <gauravbhadauria@Gauravs-MacBook-Pro.local>
2022-11-22 23:19:53 +0530

    all code

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
-import { View, Text } from 'react-native'
-import React,{useEffect} from 'react'
-import Contacts from 'react-native-contacts';
+import {View, Text} from 'react-native';
+import React from 'react';
+import AppNavigator from './src/AppNavigator';
 
 const App = () => {
+  return <AppNavigator />;
+};
 
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
-
-export default App
+export default App;
