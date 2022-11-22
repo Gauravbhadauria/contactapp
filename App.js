e164b147a9020b894a5ca129e3ae6930d6e80e9f
Gaurav Bhadauria <gauravbhadauria@Gauravs-MacBook-Pro.local>
2022-11-22 19:03:20 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React,{useEffect} from 'react'
+import Contacts from 'react-native-contacts';
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
